9f3c8192752124383e941a969051e1c5d6c6b639
warrenshiv <warrenshiv@gmail.com>
2024-09-05 22:21:53 +0300

    feat: Add Alchemy mainnet configuration to hardhat.config.js
    This commit adds the configuration for the Ethereum mainnet using Alchemy in the `hardhat.config.js` file. It includes the addition of the `mainnet` network with the Alchemy Mainnet URL loaded from the `.env` file and the private key loaded from the `.env` file. Additionally, a specific gas price of 50 Gwei is set for the network.

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,13 +1,26 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config();
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.24",
   networks: {
+    // Configuration for Hardhat's built-in local network
     hardhat: {
       localhost: {
         url: "http://localhost:8545",
       },
     },
+
+    // Configuration for Ethereum mainnet using Alchemy
+    mainnet: {
+      url: process.env.ALCHEMY_API_URL, // Load Alchemy Mainnet URL from .env file
+      accounts: [`0x${process.env.PRIVATE_KEY}`], // Load private key from .env file
+      gasPrice: 50000000000, // Optional: Set a specific gas price (50 Gwei), adjust if needed
+    },
+  },
+
+  etherscan: {
+    apiKey: process.env.ETHERSCAN_API_KEY, // Load Etherscan API key from .env file
   },
 };
